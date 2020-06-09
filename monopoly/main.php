b0e266b147cc1d3ac146eb248e3cf77d179b3ac7
lucasmercier49 <60919663+lucasmercier49@users.noreply.github.com>
2020-06-09 13:48:30 +0200

    Update main.php

--- monopoly/main.php
+++ monopoly/main.php
@@ -4,6 +4,7 @@ include "Player.php";
 $Dbo = new Player("Dbo", [], 10000, []);
 $Enzo = new Player("Enzo", [], 10000, []);
 $Paris = new City("Paris", 500, 200);
+$Angers = new City("Angers", 200, 200);
 
 
 $Dbo->payPlayer($Enzo, 50000);
